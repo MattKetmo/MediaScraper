95f2132bd26587614db3707840ee6cbcb57cefd4
Matthieu Moquet <matthieu.moquet@comuto.com>
2011-10-23 12:46:34 +0200

    Added show scraper

---
+++ src/MediaScraper/Show.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MediaScraper;
+
+use DateTime;
+
+/**
+ * A Show
+ */
+class Show
+{
+    protected $id = array();
+    protected $title;
+    protected $year;
+    protected $plot;
+
+    // TODO
+}
