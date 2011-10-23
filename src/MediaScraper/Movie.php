490b5e33ca60cdca385c0789308fef3d19982864
Matthieu Moquet <matthieu.moquet@comuto.com>
2011-10-23 15:38:20 +0200

    Added Media abstract object

--- src/MediaScraper/Movie.php
+++ src/MediaScraper/Movie.php
@@ -7,41 +7,13 @@ use DateTime;
 /**
  * A movie
  */
-class Movie
+class Movie extends Media
 {
-    protected $id = array();
+    const IMDB_IDENTIFIER = 'imdb';
+
     protected $title;
     protected $year;
     protected $plot;
-
-    /**
-     * Get the ID attached to the $key
-     *
-     * @param string $key The key/identifier of the ID
-     *
-     * @return mixed The ID
-     */
-    public function getId($key)
-    {
-        if (!array_key_exists($key, $this->id)) {
-            return false;
-        }
-
-        return $this->id[$key];
-    }
-
-    /**
-     * Set the ID attached to the $key
-     *
-     * @param string $key The key/identifier of the ID
-     * @param mixed  $id  The ID
-     *
-     * @return void
-     */
-    public function setId($key, $id)
-    {
-        $this->id[$key] = $id;
-    }
 
     /**
      * Get the IMDb ID of this movie
@@ -50,7 +22,7 @@ class Movie
      */
     public function getImdbId()
     {
-        return $this->getId('imdb');
+        return $this->getId(self::IMDB_IDENTIFIER);
     }
 
     /**
@@ -62,7 +34,7 @@ class Movie
      */
     public function setImdbId($id)
     {
-        $this->setId('imdb', $id);
+        $this->setId(self::IMDB_IDENTIFIER, $id);
     }
 
     /**
--- src/MediaScraper/Show.php
+++ src/MediaScraper/Show.php
@@ -7,12 +7,99 @@ use DateTime;
 /**
  * A Show
  */
-class Show
+class Show extends Media
 {
-    protected $id = array();
+    const IMDB_IDENTIFIER = 'imdb';
+
     protected $title;
     protected $year;
     protected $plot;
 
-    // TODO
+    /**
+     * Get the IMDb ID of this movie
+     *
+     * @return mixed IMDb ID
+     */
+    public function getImdbId()
+    {
+        return $this->getId(self::IMDB_IDENTIFIER);
+    }
+
+    /**
+     * Set the IMDb ID of the movie
+     *
+     * @param mixed  $id  The IMDb ID (ttXXXXXXX)
+     *
+     * @return void
+     */
+    public function setImdbId($id)
+    {
+        $this->setId(self::IMDB_IDENTIFIER, $id);
+    }
+
+    /**
+     * Get the title of the movie
+     *
+     * @return string The title of the movie
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the title of the movie
+     *
+     * @param string $title The title of the movie
+     *
+     * @return void
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get the year of the movie
+     *
+     * @return string The year of the movie
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * Set the year of the movie
+     *
+     * @param string $year The year of the movie
+     *
+     * @return void
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+    }
+
+    /**
+     * Get the plot of the movie
+     *
+     * @return string The plot of the movie
+     */
+    public function getPlot()
+    {
+        return $this->plot;
+    }
+
+    /**
+     * Set the plot of the movie
+     *
+     * @param string $plot The plot of the movie
+     *
+     * @return void
+     */
+    public function setPlot($plot)
+    {
+        $this->plot = $plot;
+    }
 }
